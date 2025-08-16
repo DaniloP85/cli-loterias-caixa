86a7d3b1ee5ee15a3a11769470886fc33c94a556
Danilo Santos <danilopsnts@gmail.com>
2025-08-15 23:19:39 -0300

    add geração de arquivos

--- src/main/java/br/com/dpsnqmk/service/HttpServiceImpl.java
+++ src/main/java/br/com/dpsnqmk/service/HttpServiceImpl.java
@@ -1,6 +1,8 @@
 package br.com.dpsnqmk.service;
 
 import br.com.dpsnqmk.dto.ConcursoDTO;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
@@ -15,12 +17,34 @@ public class HttpServiceImpl implements HttpService{
 
     @Override
     @Retryable(
-            value = { HttpServerErrorException.class }, // Rechama para erros do servidor (5xx)
-            maxAttempts = 3, // Tenta até 3 vezes
-            backoff = @Backoff(delay = 1000) // Aguarda 2 segundos entre as tentativas
+            value = { HttpServerErrorException.BadGateway.class }, // Rechama para erros do servidor (5xx)
+            maxAttempts = 5, // Tenta até 3 vezes
+            backoff = @Backoff(delay = 1000, multiplier = 2) // 1s, depois 2s, depois 4s...
     )
     public ConcursoDTO recuperarConcurso(String url) {
-        return restTemplate.getForObject(url, ConcursoDTO.class);
+        try {
+            ResponseEntity<ConcursoDTO> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    null,
+                    ConcursoDTO.class
+            );
+
+            return response.getBody();
+        } catch (HttpServerErrorException e) {
+            int statusCode = e.getStatusCode().value();
+
+            if (statusCode == 502) {
+                System.err.println("Erro 502 - Bad Gateway");
+                // Faça algo específico para 502
+            }
+            else if (statusCode == 503) {
+                System.err.println("Erro 503 - Service Unavailable");
+                // Faça algo específico para 503
+            }
+
+            throw e; // Repropaga a exceção para o @Retryable ou @Recover
+        }
     }
 
     @Recover
